dec12f3b72896506db767637a139182e321e2c52
anilika <cute.red.box@gmail.com>
2016-10-16 19:52:46 +1000

    Added check for call with argument.

--- spec/black_jack/dealer_spec.rb
+++ spec/black_jack/dealer_spec.rb
@@ -6,8 +6,15 @@ describe 'Dealer' do
   end
 
   describe '#deal_card' do
-    it 'returns card' do
-      expect(@dealer.deal_card).to be_instance_of(BlackJack::Card)
+    context 'when pass an argument' do
+      it 'returns a number of cards which was passed as array of object' do
+        expect(@dealer.deal_card(2).size).to eq(2)
+      end
+    end
+    context 'when pass no argument' do
+      it 'returns one card as object' do
+        expect(@dealer.deal_card).to be_instance_of(BlackJack::Card)
+      end
     end
   end
 end
