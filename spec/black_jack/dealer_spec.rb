ac1b58a301486ac1a55f1218058c0055a978e332
anilika <cute.red.box@gmail.com>
2016-10-16 20:04:20 +1000

    Testing attribute for dealer spec was added.

--- spec/black_jack/dealer_spec.rb
+++ spec/black_jack/dealer_spec.rb
@@ -5,6 +5,20 @@ describe 'Dealer' do
     @dealer = BlackJack::Dealer.new
   end
 
+  describe 'attributes' do
+    before (:all) do
+      @card = BlackJack::Card.new('As', [11, 1])
+    end
+
+    it 'allows reading for :cards' do
+      @dealer.cards.push(@card)
+      expect(@dealer.cards).to eq([@card])
+    end
+    it 'not allows writing for :cards' do
+      expect { @dealer.cards = @card }.to raise_error(NoMethodError)
+    end
+  end
+
   describe '#deal_card' do
     context 'when pass an argument' do
       it 'returns a number of cards which was passed as array of object' do
