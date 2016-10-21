bc836ed4da0f2f47dab8603c0bdd4b4284aba4e4
anilika <cute.red.box@gmail.com>
2016-10-21 13:57:12 +1000

    Test for method split was added.

--- spec/black_jack/rules_spec.rb
+++ spec/black_jack/rules_spec.rb
@@ -2,11 +2,20 @@ require 'black_jack/rules'
 
 describe 'Rules' do
   before(:all) do
-    class TestRules
+    class TestDealer
       attr_reader :card_deсk
-      include Rules
+      include Rules::Dealer
     end
-    @rules = TestRules.new
+
+    class TestPlayer
+      attr_accessor :cards
+      include Rules::Player
+      def initialize
+        @cards = []
+      end
+    end
+    @dealer = TestRules.new
+    @player = TestPlayer.new
   end
 
   describe '#rate_valid?' do
@@ -16,19 +25,19 @@ describe 'Rules' do
 
     context 'when rate more then minimum rate and less or equal player cash' do
       it 'returns rate' do
-        expect(@rules.make_rate(20, @player.cash)).to eq(20)
+        expect(@dealer.make_rate(20, @player.cash)).to eq(20)
       end
     end
 
     context 'when rate less then minimum rate' do
       it 'returns false' do
-        expect(@rules.rate_valid?(2, @player.cash)).to be_falsey
+        expect(@dealer.rate_valid?(2, @player.cash)).to be_falsey
       end
     end
 
     context 'when rate more then cash' do
       it 'returns false' do
-        expect(@rules.make_rate(200, @player.cash)).to be_falsey
+        expect(@dealer.make_rate(200, @player.cash)).to be_falsey
       end
     end
   end
@@ -39,7 +48,7 @@ describe 'Rules' do
         cards = []
         cards << BlaskJack::Card.new('As', [11, 1])
         cards << BlaskJack::Card.new('Ab', [11, 1])
-        expect(@rules.calculate_cards(cards)).to eq([2, 12])
+        expect(@dealer.calculate_cards(cards)).to eq([2, 12])
       end
     end
     context 'when the sum is one' do
@@ -47,7 +56,7 @@ describe 'Rules' do
         cards = []
         cards << BlaskJack::Card.new('9s', [9])
         cards << BlaskJack::Card.new('10s', [10])
-        expect(@rules.calculate_cards(cards)).to eq([19])
+        expect(@dealer.calculate_cards(cards)).to eq([19])
       end
     end
   end
@@ -61,40 +70,70 @@ describe 'Rules' do
 
     context 'when max of sums cards less then 21' do
       it 'returns :no' do
-        expect(@rules.hand_win?(@card)).to eq(:no)
+        expect(@dealer.hand_win?(@card)).to eq(:no)
       end
     end
     context 'when one of sums cards equal to 21' do
       it 'returns :yes' do
         @cards << BlackJack::Card.new('9s', [9])
-        expect(@rules.hand_win?(@cards)).to eq(:yes)
+        expect(@dealer.hand_win?(@cards)).to eq(:yes)
       end
     end
     context 'when min of sums cards more then 21' do
       it 'returns :bust' do
         @cards << BlackJack::Card.new('10s', [10])
         @cards << BlackJack::Card.new('10b', [10])
-        expect(@rules.hand_win?(@cards)).to eq(:bust)
+        expect(@dealer.hand_win?(@cards)).to eq(:bust)
       end
     end
   end
 
   describe '#create_card_deck' do
     before(:all) do
-      @rules.create_card_deck
+      @dealer.create_card_deck
     end
 
     it 'creates variable @card_deсk' do
-      expect(@rules.card_deсk.empty?).to be_falsey
+      expect(@dealer.card_deсk.empty?).to be_falsey
     end
     it 'puts in variable @card_deck array of cards' do
-      expect(@rules.card_deck).to be_an(Array)
+      expect(@dealer.card_deck).to be_an(Array)
     end
     it 'creates 42 cards' do
-      expect(@rules.card_deсk.size).to eq(42)
+      expect(@dealer.card_deсk.size).to eq(42)
     end
     it 'creates cards which are instance of class Card' do
-      expect(@rules.card_deсk.sample).to be_instance_of(Card)
+      expect(@dealer.card_deсk.sample).to be_instance_of(Card)
+    end
+  end
+
+  describe '#split' do
+    before(:each) do
+      @player.cards = []
+    end
+
+    context 'when player is allowed to split' do
+      it 'returns cards as array of arrays' do
+        @player.cards << BlaskJack::Card.new('As', [11, 1])
+        @player.cards << BlaskJack::Card.new('Ab', [11, 1])
+        expect(@player.split).to eq([[cards[0]], [cards[1]]])
+      end
+    end
+
+    caontext 'when player is not allowed to split' do
+      it 'returns false' do
+        @player.cards << BlaskJack::Card.new('8s', [8])
+        @player.cards << BlaskJack::Card.new('Ab', [11, 1])
+        expect(@player.split).to be_falsey
+      end
+    end
+
+    context 'when pass with argument' do
+      it 'splits that hand the number of which has been passed and returns all hands of player' do
+        cards = [[BlaskJack::Card.new(['7s', [7]]), BlaskJack::Card.new(['10a', [10]])], [BlaskJack::Card.new('7a', [7]), BlaskJack::Card.new('7b', [7])]]
+        @player.cards = cards
+        expect(@player.split(1)).to eq([cards[0], [cards[1][0]], [cards[1][1]]])
+      end
     end
   end
 end
