beda88c740cb3f4af2fd2e064ff1783e22c78be1
anilika <cute.red.box@gmail.com>
2016-10-21 14:42:15 +1000

    Class Player was added with initialize.

--- lib/black_jack/player.rb
+++ lib/black_jack/player.rb
@@ -1,5 +1,11 @@
 module BlackJack
   class Player
+    attr_reader :name, :cash, :rate
 
+    def initialize(name, cash)
+      @name = name
+      @cash = cash
+      @rate = 0
+    end
   end
 end
