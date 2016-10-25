48280ae91858c4fc4dd8a6d1aff1b131fb606e0e
anilika <cute.red.box@gmail.com>
2016-10-26 01:28:06 +1000

    Class Dealer was completed.

--- lib/black_jack/dealer.rb
+++ lib/black_jack/dealer.rb
@@ -1,10 +1,22 @@
+require_relative 'card_deck'
+require_relative 'rules/rules'
+
 module BlackJack
   class Dealer
     attr_reader :cards, :card_deck
 
     def initialize
       @cards = []
-      @card_deck = create_card_deck
+      @card_deck = CardDeck.new
+      @card_deck.shuffle
+    end
+
+    def deal_card
+      @card_deck.upper_card
+    end
+
+    def give_win(situation, bet)
+      bet * Rules::BET[situation]
     end
   end
 end
--- spec/black_jack/dealer_spec.rb
+++ spec/black_jack/dealer_spec.rb
@@ -21,15 +21,16 @@ describe 'Dealer' do
   end
 
   describe '#deal_card' do
-    context 'when pass an argument' do
-      it 'returns a number of cards which was passed as array of object' do
-        expect(@dealer.deal_card(2).size).to eq(2)
-      end
+    it 'returns one card as object' do
+      expect(@dealer.deal_card).to be_instance_of(BlackJack::Card)
     end
-    context 'when pass no argument' do
-      it 'returns one card as object' do
-        expect(@dealer.deal_card).to be_instance_of(BlackJack::Card)
-      end
+  end
+
+  describe '#give_win' do
+    it 'takes a bet and situation and returns bet multiplied by coefficient of situation' do
+      bet = 20
+      expect(@dealer.give_win(:black_jack, bet)).to eq(30)
+      expect(@dealer.give_win(:push, bet)).to eq(20)
     end
   end
 end
