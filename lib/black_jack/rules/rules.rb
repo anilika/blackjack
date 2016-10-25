f04b57c51dbb3b1e4e83bd15d4eb68a45b28c441
anilika <cute.red.box@gmail.com>
2016-10-26 01:29:32 +1000

    To module Rules was added constant BET and test for its.

--- lib/black_jack/rules/rules.rb
+++ lib/black_jack/rules/rules.rb
@@ -1,6 +1,8 @@
 module BlackJack
   module Rules
     MIN_BET = 10
+    MIN_CASH = 0
     BLACK_JACK = 21
+    BET = { black_jack: 1.5, push: 1 }.freeze
   end
 end
--- spec/black_jack/rules/rules_spec.rb
+++ spec/black_jack/rules/rules_spec.rb
@@ -1,15 +1,20 @@
-require 'black_jack/rules'
+require 'black_jack/rules/rules'
 
 describe 'Rules' do
   describe 'constants' do
     describe 'MIN_BET' do
       it 'returns value of minimum bet' do
-        expect(BlackJack::Rules::MIN_BET).to eq(20)
+        expect(BlackJack::Rules::MIN_BET).to eq(10)
       end
     end
     describe 'MIN_CASH' do
       it 'returns minimum value for player cash' do
         expect(BlackJack::Rules::MIN_CASH).to eq(0)
+      end
+    end
+    describe 'BLACK_JACK' do
+      it 'returns number of points for BlackJack' do
+        expect(BlackJack::Rules::BLACK_JACK).to eq(21)
       end
     end
   end
