3f0ffc27a291cacdf1a65acc7a58901ea58e1380
anilika <cute.red.box@gmail.com>
2016-10-25 22:51:01 +1000

    Methods bet_valid? and make_bet_if_valid was added.

--- lib/black_jack/player.rb
+++ lib/black_jack/player.rb
@@ -1,3 +1,6 @@
+require_relative 'hand'
+require_relative 'rules/rules'
+
 module BlackJack
   class Player
     attr_reader :name, :cash, :bet, :hands
@@ -11,12 +14,29 @@ module BlackJack
 
     def make_rate(rate)
       @cash -= rate
-      @rate = rate
+      @bet = bet
     end
 
     def take_win(win)
       @cash += win
     end
 
+    def make_bet_if_valid(bet)
+      if bet_valid?(bet)
+        hands.first.add_bet(bet)
+        true
+      else
+        false
+      end
+    end
+
+    def bet_valid?(bet)
+      begin
+        bet = Integer(bet)
+      rescue
+        return false
+      end
+      bet <= cash && bet >= Rules::MIN_BET
+    end
   end
 end
