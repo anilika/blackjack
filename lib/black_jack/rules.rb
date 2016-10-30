fd9cf22575fe2d2b7bb368af42fb936ef4e4be57
anilika <cute.red.box@gmail.com>
2016-10-30 15:40:20 +1000

    Few constants was added to module Rules.

--- lib/black_jack/rules.rb
+++ lib/black_jack/rules.rb
@@ -5,5 +5,9 @@ module BlackJack
     BLACK_JACK = 21
     BET = { black_jack: 1.5, push: 1 }.freeze
     DOUBLE_SUM = [10, 11]
+    PLAYER_NAME = { more: 2, less: 11 }
+    PLAYER_CASH = { more: MIN_BET, less: 50000 }
+    PLAYER_ACTION = [:hit, :split, :double]
+    PLAYER_YES_NO = [:yes, :no]
   end
 end
