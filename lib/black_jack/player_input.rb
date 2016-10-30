41014419c3b174ddb8ada0b694c2d022709d2865
anilika <cute.red.box@gmail.com>
2016-10-30 15:41:08 +1000

    Class PlayerInput with tests was added.

---
+++ lib/black_jack/player_input.rb
@@ -0,0 +1,59 @@
+require_relative 'rules'
+require_relative 'input'
+
+module BlackJack
+  class PlayerInput
+    def self.name
+      range = (Rules::PLAYER_NAME[:more]..Rules::PLAYER_NAME[:less])
+      loop do
+        name = Input.take_input
+        next if name.split.size.zero?
+        if range.include?(name.size)
+          return name.capitalize
+        end
+      end
+    end
+
+    def self.cash
+      range = (Rules::PLAYER_CASH[:more]..Rules::PLAYER_CASH[:less])
+      loop do
+        begin
+          cash = Integer(Input.take_input)
+        rescue
+          next
+        end
+        return cash if range.include?(cash)
+      end
+    end
+
+    def self.bet(cash)
+      range = (Rules::MIN_BET..cash)
+      loop do
+        begin
+          bet = Integer(Input.take_input)
+        rescue
+          next
+        end
+        return bet if range.include?(bet)
+      end
+    end
+
+    def self.game_action
+      loop do
+        action = Input.take_input.downcase.to_sym
+        if Rules::PLAYER_ACTION.include?(action)
+          return action
+        end
+      end
+    end
+
+    def self.yes_no
+      loop do
+        action = Input.take_input.downcase.to_sym
+        if Rules::PLAYER_YES_NO.include?(action)
+          return action
+        end
+      end
+    end
+  end
+end
---
+++ spec/black_jack/player_input_spec.rb
@@ -0,0 +1,63 @@
+require 'black_jack/player_input'
+
+describe 'PlayerInput' do
+  describe '.name' do
+    it 'returns passed name' do
+      module BlackJack::Input
+        NAMES = ['a', 'qasdewxzcvfr', '     ', 'john']
+        def self.take_input
+          NAMES.shift
+        end
+      end
+      expect(BlackJack::PlayerInput.name).to eq('John')
+    end
+  end
+
+  describe '.cash' do
+    it 'returns passed cash' do
+      module BlackJack::Input
+        CASH = ['0', 'dfsf', 'd', 'SdsfSS', 'SS', '1000000', '1', '100']
+        def self.take_input
+          CASH.shift
+        end
+      end
+      expect(BlackJack::PlayerInput.cash).to eq(100)
+    end
+  end
+
+  describe '.bet' do
+    it 'returns passed bet' do
+      module BlackJack::Input
+        BET = ['sswd', '1', '10000000000', ' ', '', '25']
+        def self.take_input
+          BET.shift
+        end
+      end
+      expect(BlackJack::PlayerInput.bet(100)).to eq(25)
+    end
+  end
+
+  describe '.game_action' do
+    it 'returns passed game action' do
+      module BlackJack::Input
+        ACTIONS = ['sdsa', '123', 'yes', 'Doouuble', 'hit']
+        def self.take_input
+          ACTIONS.shift
+        end
+      end
+      expect(BlackJack::PlayerInput.game_action).to eq(:hit)
+    end
+  end
+
+  describe '.yes_no' do
+    it 'returns passed yes or no' do
+      module BlackJack::Input
+        ACTIONS = ['sdsa', '123', 'hit', 'yes']
+        def self.take_input
+          ACTIONS.shift
+        end
+      end
+      expect(BlackJack::PlayerInput.yes_no).to eq(:yes)
+    end
+  end
+end
